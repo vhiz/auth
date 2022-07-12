949adc25ea72549da6524005bcab52dc801f950b
Mgbeahurike Victor <104136490+vhiz@users.noreply.github.com>
2022-07-12 21:27:01 +0100

    change

--- routes/login.js
+++ routes/login.js
@@ -36,7 +36,7 @@ router.post('/login', async(req,res)=>{
 
     // create a token
     const token = jwt.sign({_id:user._id}, process.env.TOKEN, {expiresIn: '24h'})
-    res.header('auth', token).send(token)
+    res.header('auth', token).send(token, user)
     console.log('loged in')
 })
 
